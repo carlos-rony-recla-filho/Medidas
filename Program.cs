328407691f90be4336cf4af5edc623da86c2a4eb
Carlos Rony <carlosrecla03@gmail.com>
2021-09-20 18:23:36 -0300

    finaldo trabalho

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace medidas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double metros = 0;
+            Double cm = 0;
+            Double km = 0;
+
+            Console.WriteLine("Bem-vindo ao conversorde metros em Km e Cm");
+            
+            Console.Write("\n");
+            
+            Console.Write("Digite a medida em metros: ");
+            metros = Int32.Parse(Console.ReadLine());
+
+            cm = metros * 100;
+            km = metros / 1000;
+            
+            Console.WriteLine($"--- Equivalência ---\n {cm} cm \n {metros} m \n {km} km");
+        }
+
+    }
+}
